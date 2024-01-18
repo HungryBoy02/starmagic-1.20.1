ab46b200bd981d35f5fe37f7d8e2cc8cbcfbe0c9
HungryBoy02 <hungryboy02@gmail.com>
2024-01-17 18:42:57 -0600

    Initial commit

---
+++ src/main/java/net/hungryfox/starmagic/StarmagicClient.java
@@ -0,0 +1,10 @@
+package net.hungryfox.starmagic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StarmagicClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hungryfox/starmagic/StarmagicDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hungryfox.starmagic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StarmagicDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
